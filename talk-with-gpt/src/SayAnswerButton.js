3823a70897f1943421c1ec2512db3f2f8a83920b
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-22 13:29:54 +0100

    reafktor - uproszczenie kodu

--- talk-with-gpt/src/SayAnswerButton.js
+++ talk-with-gpt/src/SayAnswerButton.js
@@ -2,28 +2,30 @@ import React from "react";
 import RecordVoiceOverIcon from "@mui/icons-material/RecordVoiceOver";
 import { Button } from "@mui/material";
 
+function dataToBase64(dataArray) {
+    return btoa(String.fromCharCode.apply(null, dataArray));
+}
+
+async function fetchAndPlayAudio(answer) {
+    const response = await fetch("http://localhost:5000/text-to-speech", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: answer }),
+    });
+
+    const data = await response.json();
+    const base64Data = dataToBase64(data.audioContent.data);
+
+    const audio = new Audio();
+    audio.src = `data:audio/mp3;base64,${base64Data}`;
+    audio.play();
+}
+
 export default function SayAnswerButton({ answer }) {
     const handleClick = () => {
-        fetch(`http://localhost:5000/text-to-speech`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ text: answer }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-
-                // Konwertuj dane audio na base64
-                const base64Data = btoa(
-                    String.fromCharCode.apply(null, data.audioContent.data)
-                );
-
-                const audio = new Audio();
-                audio.src = `data:audio/mp3;base64,${base64Data}`;
-                audio.play();
-            });
+        fetchAndPlayAudio(answer);
     };
 
     return (
