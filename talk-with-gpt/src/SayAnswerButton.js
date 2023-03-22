4796d0e4e76c8ff74cec07e4001f3876eae2c80f
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-22 13:27:49 +0100

    używanie syntezatora mowy google

--- talk-with-gpt/src/SayAnswerButton.js
+++ talk-with-gpt/src/SayAnswerButton.js
@@ -1,13 +1,30 @@
 import React from "react";
-import RecordVoiceOverIcon from '@mui/icons-material/RecordVoiceOver';
+import RecordVoiceOverIcon from "@mui/icons-material/RecordVoiceOver";
 import { Button } from "@mui/material";
 
 export default function SayAnswerButton({ answer }) {
     const handleClick = () => {
-        const utterance = new SpeechSynthesisUtterance(answer);
-        utterance.lang = 'pl-PL';
-        speechSynthesis.speak(utterance);
-    }
+        fetch(`http://localhost:5000/text-to-speech`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ text: answer }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+
+                // Konwertuj dane audio na base64
+                const base64Data = btoa(
+                    String.fromCharCode.apply(null, data.audioContent.data)
+                );
+
+                const audio = new Audio();
+                audio.src = `data:audio/mp3;base64,${base64Data}`;
+                audio.play();
+            });
+    };
 
     return (
         <Button
